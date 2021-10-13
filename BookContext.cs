0bfd5a5e0e7af26e8c57ccdc820097fbf2c9e67d
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-13 03:22:54 +0200

    Functions

---
+++ BookContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionApp39
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
