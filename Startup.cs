58f303cb1646c417141d6f5c2e2926a8836fec37
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-13 03:24:33 +0200

    Functions

---
+++ Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(FunctionApp39.Startup))]
+
+namespace FunctionApp39
+{
+    class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            string SqlConnection = Environment.GetEnvironmentVariable("SqlConnectionString");
+            builder.Services.AddDbContext<BookContext>(
+                options => options.UseSqlServer(SqlConnection));
+        }
+    }
+}
