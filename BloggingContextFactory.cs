23e6498f3209fe5bf0a52d303bfc8a50df7da0f1
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-13 03:25:17 +0200

    Functions

---
+++ BloggingContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionApp38
+{
+    public class BloggingContextFactory : IDesignTimeDbContextFactory<BookContext>
+    {
+        public BookContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BookContext>();
+            optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("SqlConnectionString"));
+
+            return new BookContext(optionsBuilder.Options);
+        }
+    }
+}
